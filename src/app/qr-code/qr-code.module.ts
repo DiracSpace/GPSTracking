e190bbe6024639faa1def42a3a25ca43b6a69457
Roberto de León <49464343+DiracSpace@users.noreply.github.com>
2022-08-26 15:37:56 -0700

    QR Codes (#4)
    * Installed and generated QR module
    
    * Base setup of Qr Viewer

---
+++ src/app/qr-code/qr-code.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QrCodeViewerComponent } from './qr-code-viewer/qr-code-viewer.component';
+
+@NgModule({
+  declarations: [QrCodeViewerComponent],
+  imports: [CommonModule],
+})
+export class QrCodeModule {}
