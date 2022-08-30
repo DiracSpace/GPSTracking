8d1c47f14aea768295bd120509def02f2d329cd5
Roberto de León <49464343+DiracSpace@users.noreply.github.com>
2022-08-30 04:06:50 -0500

    QR code generation are now working (#13)
    Co-authored-by: DiracSpace <jayson.roberto.deleon@gmail.com>

--- src/app/qr-code/qr-code-viewer/qr-code-viewer.component.ts
+++ src/app/qr-code/qr-code-viewer/qr-code-viewer.component.ts
@@ -1,14 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    ViewChild
+} from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
+import { Logger, LogLevel } from 'src/app/logger';
+
+const logger = new Logger({
+    source: 'QrCodeViewerComponent',
+    level: LogLevel.Debug
+});
 
 @Component({
-  selector: 'app-qr-code-viewer',
-  templateUrl: './qr-code-viewer.component.html',
-  styleUrls: ['./qr-code-viewer.component.scss'],
+    selector: 'app-qr-code-viewer',
+    templateUrl: './qr-code-viewer.component.html',
+    styleUrls: ['./qr-code-viewer.component.scss']
 })
 export class QrCodeViewerComponent implements OnInit {
+    @ViewChild('qrCodeElement', { static: true })
+    qrCodeElement: any;
 
-  constructor() { }
+    @Input() qrCodeInformation: string = '';
+    @Output() qrCodeSrc = new EventEmitter<string>();
 
-  ngOnInit() {}
+    constructor() {}
 
+    ngOnInit() {}
+
+    get imgSrc() {
+        if (!this.qrCodeElement) {
+            logger.log('Could not get qrCodeElement');
+            return null;
+        }
+
+        const img = this.qrCodeElement.qrcElement.nativeElement.querySelector('img');
+
+        if (!img) {
+            logger.log('Could not get qrCodeElement img tag');
+            return null;
+        }
+
+        return img.src;
+    }
+
+    onChangeURL(url: SafeUrl) {
+        if (this.qrCodeElement) {
+            this.qrCodeSrc.emit(this.imgSrc);
+        }
+    }
+
+    onDownloadClicked() {
+        if (this.imgSrc) {
+            logger.log('this.imgSrc:', this.imgSrc);
+
+            // converts base 64 encoded image to blobData
+            let blobData = this.convertBase64ToBlob(this.imgSrc);
+            // saves as image
+            const blob = new Blob([blobData], { type: 'image/png' });
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            // name of the file
+            link.download = 'Qrcode';
+            link.click();
+        }
+    }
+
+    private convertBase64ToBlob(Base64Image: string) {
+        // split into two parts
+        const parts = Base64Image.split(';base64,');
+        // hold the content type
+        const imageType = parts[0].split(':')[1];
+        // decode base64 string
+        const decodedData = window.atob(parts[1]);
+        // create unit8array of size same as row data length
+        const uInt8Array = new Uint8Array(decodedData.length);
+        // insert all character code into uint8array
+        for (let i = 0; i < decodedData.length; ++i) {
+            uInt8Array[i] = decodedData.charCodeAt(i);
+        }
+        // return blob image after conversion
+        return new Blob([uInt8Array], { type: imageType });
+    }
 }
---
+++ src/app/utils/images.ts
@@ -0,0 +1,9 @@
+export function blobToBase64(blob: Blob): Promise<string> {
+    return new Promise((resolve, _) => {
+        const reader = new FileReader();
+        const result = reader.result as string;
+        console.log("reader.result:", reader.result);
+        reader.onloadend = () => resolve(result);
+        reader.readAsDataURL(blob);
+    });
+}
