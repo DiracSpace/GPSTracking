9ff25415ddc6cd2920d42720e242dd2ead7fb1b0
viex <carp_97@outlook.com>
2022-08-30 12:15:09 -0500

    Updated AndroidPermissionsUtils

--- src/app/services/android-permissions-utils.service.ts
+++ src/app/services/android-permissions-utils.service.ts
@@ -5,76 +5,63 @@ import { Debugger } from '../core/components/debug/debugger.service';
 import { decodeErrorDetails } from '../utils/errors';
 
 @Injectable({ providedIn: 'root' })
-export class AndroidPermissionUtils {
+export class AndroidPermissionsUtils {
     constructor(
         private debug: Debugger,
         private androidPermissions: AndroidPermissions,
         private locationAccuracy: LocationAccuracy
     ) {}
 
-    async hasPermissionAsync(permission: any): Promise<boolean> {
+    async hasAndroidPermissionAsync(permission: any): Promise<boolean> {
         this.debug.info(`Checking permission ${permission}...`);
 
-        try {
-            const result = await this.androidPermissions.checkPermission(permission);
+        const result = await this.androidPermissions.checkPermission(permission);
 
-            if (result.hasPermission) {
-                this.debug.info('Permission is granted :D');
-                return true;
-            } else {
-                this.debug.info('Permission has not been granted :(');
-                return false;
-            }
-        } catch (error) {
-            const details = decodeErrorDetails(error);
-            this.debug.error('Permission could not be checked');
-            this.debug.error(details.toString());
-            throw error;
+        if (result.hasPermission) {
+            this.debug.info('Permission is already granted :D');
+        } else {
+            this.debug.info('Permission has not been granted :(');
         }
+
+        return result.hasPermission;
     }
 
-    async requestPermissionAsync(permission: any): Promise<boolean> {
+    async requestAndroidPermissionAsync(permission: any): Promise<boolean> {
         this.debug.info(`Requesting permission ${permission}...`);
 
-        try {
-            const canRequest = await this.locationAccuracy.canRequest();
+        const result = await this.androidPermissions.requestPermission(permission);
 
-            if (canRequest) {
-                this.debug.info('Permission is already granted');
-                return true;
-            }
-
-            this.debug.info('Permission has not been granted yet');
-
-            const result = await this.androidPermissions.requestPermission(permission);
-
-            if (result.hasPermission) {
-                this.debug.info('Permission approved :D');
-                return true;
-            } else {
-                this.debug.info('Permission denied :(');
-                return false;
-            }
-        } catch (error) {
-            const details = decodeErrorDetails(error);
-            this.debug.error('Permission could not be requested');
-            this.debug.error(details.toString());
-            throw error;
+        if (result.hasPermission) {
+            this.debug.info('Permission approved :D');
+        } else {
+            this.debug.info('Permission denied :(');
         }
+
+        return result.hasPermission;
+    }
+
+    /* #region GPS */
+
+    async canRequestLocationAccuracy(): Promise<boolean> {
+        this.debug.info('Checking location accuracy permission...');
+
+        const canRequest = await this.locationAccuracy.canRequest();
+
+        if (canRequest) {
+            this.debug.info('Permission can be requested :D');
+        } else {
+            this.debug.info('Permission cannot be requested :(');
+        }
+
+        return canRequest;
     }
 
     async turnOnGpsAsync() {
         this.debug.info('Turning on gps...');
-
-        try {
-            const accuracy = this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY;
-            await this.locationAccuracy.request(accuracy);
-            this.debug.info('Turned on gps :D');
-        } catch (error) {
-            const details = decodeErrorDetails(error);
-            this.debug.error('Could not turn on gps :(');
-            this.debug.error(details.toString());
-            throw error;
-        }
+        const accuracy = this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY;
+        await this.locationAccuracy.request(accuracy);
+        this.debug.info('Turned on gps :D');
     }
+
+    /* #endregion */
 }
